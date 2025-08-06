47c17da02c25b0463e1df1445bf9238008538b3b
Mohammed Puthawala <mohammedputhawala793@gmail.com>
2025-08-06 15:35:39 +0530

    first

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export const Navbar = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  let scrollTimeout: ReturnType<typeof setTimeout>;
+
+  const controlNavbar = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+
+    setLastScrollY(currentScrollY);
+
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(() => {
+      setIsVisible(true);
+    }, 200);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <nav
+      className={`sticky top-0 z-50 transition-transform duration-300 ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      } flex w-full items-center justify-between border-t border-b border-neutral-200 bg-white px-4 py-4 dark:border-neutral-800 dark:bg-black`}
+    >
+      <div className="flex items-center gap-2">
+        <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
+        <h1 className="text-base font-bold md:text-2xl">IT Company</h1>
+      </div>
+      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+        Login
+      </button>
+    </nav>
+  );
+};
