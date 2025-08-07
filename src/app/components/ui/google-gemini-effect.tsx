f62e5e47af18b6dc2c72ec59f9abfeed273fd9d1
Mohammed Puthawala <mohammedputhawala793@gmail.com>
2025-08-07 15:15:18 +0530

    fix

--- src/app/components/ui/google-gemini-effect.tsx
+++ src/app/components/ui/google-gemini-effect.tsx
@@ -29,9 +29,9 @@ export const GoogleGeminiEffect = ({
           `Accelerate autonomous growth with our custom-built AI agents.`}
       </p>
       <div className="w-full h-[890px] -top-60 md:-top-40  flex items-center justify-center bg-red-transparent absolute ">
-        <button className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto ">
+        <a href="tel:+919586091534" className="font-bold bg-white rounded-full md:px-4 md:py-2 px-2 py-1 md:mt-24 mt-8 z-30 md:text-base text-black text-xs  w-fit mx-auto ">
           Let&apos;s Connect
-        </button>
+        </a>
       </div>
       <svg
         width="1440"
